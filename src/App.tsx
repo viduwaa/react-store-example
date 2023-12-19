8c3353492af984ccc5b004c765e8d889f4e15e7a
Vidula Deneth <viduladenethsalwathura@gmail.com>
2023-12-19 11:49:25 +0530

    fix deploy ?

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 //import { useState } from "react";
 //import { Routes, Route } from "react-router-dom";
-import { HashRouter as  Routes, Route } from "react-router-dom";
+
 import { About } from "./pages/About";
 import { Store } from "./pages/Store";
 import { NavBar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <ShoppingCartProvider>
-        <NavBar />
-        <div className="container m-auto">
-          <Routes>
-            <Route path="/store" element={<Store />}></Route>
-            <Route path="/about" element={<About />}></Route>
-          </Routes>
-        </div>
+        <Router>
+          <NavBar />
+          <div className="container m-auto">
+            <Routes>
+              <Route path="/store" element={<Store />}></Route>
+              <Route path="/about" element={<About />}></Route>
+            </Routes>
+          </div>
+        </Router>
       </ShoppingCartProvider>
     </>
   );
